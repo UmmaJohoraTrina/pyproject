b82a5918c01ff51f267ca599cb6c92e242ab2620
UmmaJohoraTrina <71620467+UmmaJohoraTrina@users.noreply.github.com>
2020-09-21 09:56:33 +0600

    Create turtelfunction.py

---
+++ turtelfunction.py
@@ -0,0 +1,19 @@
+import turtle
+
+trina_turtle=turtle.Turtle()
+def square():
+
+ trina_turtle.forward(100)
+ trina_turtle.right(90)
+ trina_turtle.forward(100)
+ trina_turtle.right(90)
+ trina_turtle.forward(100)
+ trina_turtle.right(90)
+ trina_turtle.forward(100)
+
+square()
+trina_turtle.forward(150)
+square()
+
+trina_turtle.left(90)
+square()
