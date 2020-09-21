780f45602dc0302428810a7b5179c48c13a70faf
UmmaJohoraTrina <71620467+UmmaJohoraTrina@users.noreply.github.com>
2020-09-21 09:37:19 +0600

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+trina_turtle=turtle.Turtle()
+trina_turtle.forward(100)
+trina_turtle.right(90)
+trina_turtle.forward(100)
+trina_turtle.right(90)
+trina_turtle.forward(100)
+trina_turtle.right(90)
+trina_turtle.forward(100)
